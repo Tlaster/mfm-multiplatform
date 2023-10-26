a091421425b5a29c65fb7ef509763ab4ca8adb82
Tlaster <tlaster@outlook.com>
2023-10-26 11:51:33 +0900

    fix lint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,94 +1,94 @@
 import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
-  kotlin("multiplatform") version "1.9.0"
-  id("org.jetbrains.kotlinx.kover") version "0.7.3"
-  id("com.vanniktech.maven.publish") version "0.25.3"
-  id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
+    kotlin("multiplatform") version "1.9.0"
+    id("org.jetbrains.kotlinx.kover") version "0.7.3"
+    id("com.vanniktech.maven.publish") version "0.25.3"
+    id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
 }
 
 group = "moe.tlaster"
 version = "0.1.1"
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 kotlin {
-  jvm {
-    jvmToolchain(11)
-    testRuns.named("test") {
-      executionTask.configure {
-        useJUnitPlatform()
-      }
+    jvm {
+        jvmToolchain(11)
+        testRuns.named("test") {
+            executionTask.configure {
+                useJUnitPlatform()
+            }
+        }
     }
-  }
-  js {
-    browser()
-    nodejs()
-  }
-  //    @OptIn(ExperimentalWasmDsl::class)
-  //    wasm {
-  //        browser()
-  //        nodejs()
-  //        d8()
-  //    }
-  ios()
-  iosSimulatorArm64()
-  macosX64()
-  macosArm64()
-  watchos()
-  watchosSimulatorArm64()
-  tvos()
-  tvosSimulatorArm64()
-  mingwX64()
-  linuxX64()
-  linuxArm64()
+    js {
+        browser()
+        nodejs()
+    }
+    //    @OptIn(ExperimentalWasmDsl::class)
+    //    wasm {
+    //        browser()
+    //        nodejs()
+    //        d8()
+    //    }
+    ios()
+    iosSimulatorArm64()
+    macosX64()
+    macosArm64()
+    watchos()
+    watchosSimulatorArm64()
+    tvos()
+    tvosSimulatorArm64()
+    mingwX64()
+    linuxX64()
+    linuxArm64()
 
-  sourceSets {
-    val commonMain by getting
-    val commonTest by getting {
-      dependencies {
-        implementation(kotlin("test"))
-      }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
     }
-  }
 }
 
 mavenPublishing {
-  publishToMavenCentral(SonatypeHost.S01)
-  signAllPublications()
-  coordinates(
-    groupId = "moe.tlaster",
-    artifactId = "mfm-multiplatform",
-    version = "0.1.1-SNAPSHOT",
-  )
-  pom {
-    name.set("mfm-multiplatform")
-    description.set("MFM parser")
-    url.set("https://github.com/Tlaster/mfm-multiplatform")
+    publishToMavenCentral(SonatypeHost.S01)
+    signAllPublications()
+    coordinates(
+        groupId = "moe.tlaster",
+        artifactId = "mfm-multiplatform",
+        version = "0.1.1-SNAPSHOT",
+    )
+    pom {
+        name.set("mfm-multiplatform")
+        description.set("MFM parser")
+        url.set("https://github.com/Tlaster/mfm-multiplatform")
 
-    licenses {
-      license {
-        name.set("MIT")
-        url.set("https://opensource.org/licenses/MIT")
-      }
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+        developers {
+            developer {
+                id.set("Tlaster")
+                name.set("James Tlaster")
+                email.set("tlaster@outlook.com")
+            }
+        }
+        scm {
+            url.set("https://github.com/Tlaster/mfm-multiplatform")
+            connection.set("scm:git:git://github.com/Tlaster/mfm-multiplatform.git")
+            developerConnection.set("scm:git:git://github.com/Tlaster/mfm-multiplatform.git")
+        }
     }
-    developers {
-      developer {
-        id.set("Tlaster")
-        name.set("James Tlaster")
-        email.set("tlaster@outlook.com")
-      }
-    }
-    scm {
-      url.set("https://github.com/Tlaster/mfm-multiplatform")
-      connection.set("scm:git:git://github.com/Tlaster/mfm-multiplatform.git")
-      developerConnection.set("scm:git:git://github.com/Tlaster/mfm-multiplatform.git")
-    }
-  }
 }
 
 ktlint {
-  version.set("0.50.0")
+    version.set("0.50.0")
 }
