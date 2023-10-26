483c3c93f3a22d341647b38bb60ee67e349ae8d9
Tlaster <tlaster@outlook.com>
2023-10-26 11:45:35 +0900

    update ci publish config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,144 +1,94 @@
-import org.jetbrains.kotlin.konan.target.HostManager
-import java.util.Properties
+import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
-    kotlin("multiplatform") version "1.9.0"
-    id("org.jetbrains.kotlinx.kover") version "0.7.3"
-    id("maven-publish")
-    id("signing")
-    id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
+  kotlin("multiplatform") version "1.9.0"
+  id("org.jetbrains.kotlinx.kover") version "0.7.3"
+  id("com.vanniktech.maven.publish") version "0.25.3"
+  id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
 }
 
 group = "moe.tlaster"
 version = "0.1.0"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 kotlin {
-    jvm {
-        jvmToolchain(11)
-        testRuns.named("test") {
-            executionTask.configure {
-                useJUnitPlatform()
-            }
-        }
+  jvm {
+    jvmToolchain(11)
+    testRuns.named("test") {
+      executionTask.configure {
+        useJUnitPlatform()
+      }
     }
-    js {
-        browser()
-        nodejs()
-    }
-    //    @OptIn(ExperimentalWasmDsl::class)
-    //    wasm {
-    //        browser()
-    //        nodejs()
-    //        d8()
-    //    }
-    if (HostManager.hostIsMac) {
-        ios()
-        iosSimulatorArm64()
-        macosX64()
-        macosArm64()
-        watchos()
-        watchosSimulatorArm64()
-        tvos()
-        tvosSimulatorArm64()
-    }
-    if (HostManager.hostIsMingw) {
-        mingwX64()
-    }
-    if (HostManager.hostIsLinux) {
-        linuxX64()
-        linuxArm64()
-    }
+  }
+  js {
+    browser()
+    nodejs()
+  }
+  //    @OptIn(ExperimentalWasmDsl::class)
+  //    wasm {
+  //        browser()
+  //        nodejs()
+  //        d8()
+  //    }
+  ios()
+  iosSimulatorArm64()
+  macosX64()
+  macosArm64()
+  watchos()
+  watchosSimulatorArm64()
+  tvos()
+  tvosSimulatorArm64()
+  mingwX64()
+  linuxX64()
+  linuxArm64()
 
-    sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
+  sourceSets {
+    val commonMain by getting
+    val commonTest by getting {
+      dependencies {
+        implementation(kotlin("test"))
+      }
     }
+  }
 }
 
-ext {
-    val publishPropFile = rootProject.file("publish.properties")
-    if (publishPropFile.exists()) {
-        Properties().apply {
-            load(publishPropFile.inputStream())
-        }.forEach { name, value ->
-            set(name.toString(), value)
-        }
-    } else {
-        set("signing.keyId", System.getenv("SIGNING_KEY_ID"))
-        set("signing.password", System.getenv("SIGNING_PASSWORD"))
-        set("signing.secretKeyRingFile", System.getenv("SIGNING_SECRET_KEY_RING_FILE"))
-        set("ossrhUsername", System.getenv("OSSRH_USERNAME"))
-        set("ossrhPassword", System.getenv("OSSRH_PASSWORD"))
-    }
-}
+mavenPublishing {
+  publishToMavenCentral(SonatypeHost.S01)
+  signAllPublications()
+  coordinates(
+    groupId = "moe.tlaster",
+    artifactId = "mfm-multiplatform",
+    version = "0.1.1-SNAPSHOT",
+  )
+  pom {
+    name.set("mfm-multiplatform")
+    description.set("MFM parser")
+    url.set("https://github.com/Tlaster/mfm-multiplatform")
 
-val javadocJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
-    archiveClassifier.set("javadoc")
-}
-// https://github.com/gradle/gradle/issues/26091
-val signingTasks = tasks.withType<Sign>()
-tasks.withType<AbstractPublishToMaven>().configureEach {
-    dependsOn(signingTasks)
-}
-publishing {
-    if (rootProject.file("publish.properties").exists()) {
-        signing {
-            sign(publishing.publications)
-        }
-        repositories {
-            maven {
-                val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-                val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-                url = if (version.toString().endsWith("SNAPSHOT")) {
-                    uri(snapshotsRepoUrl)
-                } else {
-                    uri(releasesRepoUrl)
-                }
-                credentials {
-                    username = project.ext.get("ossrhUsername").toString()
-                    password = project.ext.get("ossrhPassword").toString()
-                }
-            }
-        }
+    licenses {
+      license {
+        name.set("MIT")
+        url.set("https://opensource.org/licenses/MIT")
+      }
     }
-
-    publications.withType<MavenPublication> {
-        artifact(javadocJar)
-        pom {
-            name.set("mfm-multiplatform")
-            description.set("MFM parser")
-            url.set("https://github.com/Tlaster/mfm-multiplatform")
-
-            licenses {
-                license {
-                    name.set("MIT")
-                    url.set("https://opensource.org/licenses/MIT")
-                }
-            }
-            developers {
-                developer {
-                    id.set("Tlaster")
-                    name.set("James Tlaster")
-                    email.set("tlaster@outlook.com")
-                }
-            }
-            scm {
-                url.set("https://github.com/Tlaster/mfm-multiplatform")
-                connection.set("scm:git:git://github.com/Tlaster/mfm-multiplatform.git")
-                developerConnection.set("scm:git:git://github.com/Tlaster/mfm-multiplatform.git")
-            }
-        }
+    developers {
+      developer {
+        id.set("Tlaster")
+        name.set("James Tlaster")
+        email.set("tlaster@outlook.com")
+      }
     }
+    scm {
+      url.set("https://github.com/Tlaster/mfm-multiplatform")
+      connection.set("scm:git:git://github.com/Tlaster/mfm-multiplatform.git")
+      developerConnection.set("scm:git:git://github.com/Tlaster/mfm-multiplatform.git")
+    }
+  }
 }
 
 ktlint {
-    version.set("0.50.0")
+  version.set("0.50.0")
 }
